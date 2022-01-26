d5940d0b051671264044f99b46c4750dec20f3ad
isurumadumadawa <isurumadhumadhawa33072@gmail.com>
2022-01-26 14:50:27 +0530

    demo login

--- Demo login/app/src/main/java/com/example/demologin/Home.java
+++ Demo login/app/src/main/java/com/example/demologin/Home.java
@@ -1,51 +1,31 @@
 package com.example.demologin;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
-import com.google.android.material.snackbar.Snackbar;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.view.View;
-
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
-
-import com.example.demologin.databinding.ActivityHomeBinding;
 
 public class Home extends AppCompatActivity {
 
-    private AppBarConfiguration appBarConfiguration;
-    private ActivityHomeBinding binding;
+    TextView textViewUserName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+        textViewUserName = findViewById(R.id.textViewUserName);
+        Bundle b = getIntent().getExtras();
+        String userName = b.getString("userName");
+        textViewUserName.setText("Hello "+userName);
 
-        binding = ActivityHomeBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
-
-        setSupportActionBar(binding.toolbar);
-
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_home);
-        appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-
-        binding.fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
+    public void onLogout(View view){
 
-    @Override
-    public boolean onSupportNavigateUp() {
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_home);
-        return NavigationUI.navigateUp(navController, appBarConfiguration)
-                || super.onSupportNavigateUp();
+        finish();
     }
 }
